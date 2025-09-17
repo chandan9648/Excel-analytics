b49a1b1883093a83f0b53eeaae1920bb991e67d1
chandan9648 <chandankkumar156@gmail.com>
2025-09-17 16:40:15 +0530

    fix: update CORS configuration to allow requests from the production URL

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "localhost:5173",
+    origin: "https://excel-analytics-2004.netlify.app",
     credentials: true
 }));
 app.use(express.json());
