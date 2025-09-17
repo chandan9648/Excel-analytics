99a6fb18032f791dc6dc1d3bea32030eb63d0172
chandan9648 <chandankkumar156@gmail.com>
2025-09-17 16:02:26 +0530

    fix: update CORS configuration to allow specific origin and enable credentials

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,10 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: "https://excel-analytics-2004.netlify.app",
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
